63544fe4d92ab4dffbc1c3d35cf4a0540519b4f6
nhutvm <vmnhut1996@gmail.com>
2022-12-07 17:46:57 +0700

    a

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const app = express();
 const cors = require("cors");
 
 const Index = require("./controllers/index");
-const Upload = require("./controllers/upload");
-const Resize = require("./controllers/resize");
-const Capture = require("./controllers/capture");
-const Snapshot = require("./controllers/snapshot");
-const Pdf = require("./controllers/pdf");
-const Collect = require("./controllers/collect");
-const AutoTest = require("./controllers/autoTest");
+const Crawler = require("./controllers/crawler");
 
 const port = process.env.PORT || 4000;
 
@@ -19,13 +13,7 @@ app.use(bodyParser.json());
 app.use(cors("*"));
 
 app.use(Index);
-app.use(Upload);
-app.use(Resize);
-app.use(Capture);
-app.use(Snapshot);
-app.use(Pdf);
-app.use(Collect);
-app.use(AutoTest);
+app.use(Crawler);
 
 app.listen(port, () => {
   console.log("Connnecting: " + port);
