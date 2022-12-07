e8b81ee0640d315bb505706aaa4597b86595e129
nhutvm <nhutvm@namvietmedia.com.vn>
2022-12-07 23:38:14 +0700

    get price and promotion price

---
+++ controllers/details.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const router = express();
+
+const openBrowser = async () => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false,
+  });
+  const page = await browser.newPage();
+  return {
+    browser,
+    page,
+  };
+};
+
+const closeBrowser = async ({ page, browser }) => {
+  await page.close();
+  await browser.close();
+};
+
+router.get(`/product/:id`, async (req, res) => {
+  const { browser, page } = await openBrowser();
+  const product = await getProductById(page, req);
+  await closeBrowser({ page, browser });
+
+  if (product) {
+    return res.status(200).json(product);
+  }
+  else
+    return res.status(200).json({ message: "Nothing to crawl" });
+});
+
+const getProductById = async (page, req) => {
+  let id = req.params.id;
+  var categoryId = req.query.category_id;
+  
+  let url = `http://www.bloodybunnystore.com/en/product/${id}/product-${id}`;
+  if (categoryId) url += `?category_id=${categoryId}`;
+  
+  const selector = '.site-product';
+  await page.goto(url);
+  await page.waitForSelector(selector);
+  return await page.evaluate(selector => {
+    let item = document.querySelector(selector);
+    let title = item.querySelector('.product-cart-sec h1')?.innerText.trim();
+    let price = item.querySelector('.wrapper-cart .head-cart span')?.innerText.replace('฿', '').trim();
+    let promotionPrice = item.querySelector('.wrapper-cart .head-cart del')?.innerText.replace('฿', '').trim();
+    let salePercent = item.querySelector('.wrapper-cart .head-cart .label-sale')?.innerText.replace('-', '').replace('%','').trim();
+    let thumbnail = item.querySelector('.swiper-wrapper img')?.getAttribute('src').trim();
+    let remainingAmount = item.querySelector('.product-action .align-items-end')?.innerText.replace('item(s)','').trim();
+    let likes = item.querySelector('.product-top button')?.innerText.trim();
+
+    let descriptions = [];
+    item.querySelectorAll('.product-description p').forEach((des) => {
+      let desTitle = des.querySelector('strong')?.innerText.trim();
+      let desDetails = des.innerText.trim();
+      descriptions.push({title: desTitle, details : desDetails});
+    });
+
+    let tags = [];
+    item.querySelectorAll('.tags-list a').forEach((tag) => {
+      let tagName = tag.innerText.trim();
+      let tagLink = tag.getAttribute('href').trim();
+      tags.push({ name: tagName, url: tagLink });
+    });
+
+    let galleryThumbnails = [];
+    item.querySelectorAll('.gallery-thumbs img').forEach((thumb) => {
+      let url = thumb.getAttribute('src');
+      galleryThumbnails.push(url);
+    });
+
+    let relatedProducts = [];
+    item.querySelectorAll('.recommend-sec .item').forEach((relatedProduct) => {
+      let rTitle = relatedProduct.querySelector('h3')?.innerText.trim();
+      let rUrl = relatedProduct.querySelector('a')?.getAttribute('href').trim();
+      let rPrice = relatedProduct.querySelector('.list-price span')?.innerText.replace('฿', '').trim();
+      let rPromotionPrice = relatedProduct.querySelector('.list-price del')?.innerText.replace('฿', '').trim();
+      let rThumbnail = relatedProduct.querySelector('.img-wrapper img')?.getAttribute('src').trim();
+      let rSalePercent = relatedProduct.querySelector('.img-wrapper .label-sale')?.innerText.replace('-', '').replace('%','').trim();
+      let rId = rUrl.substring(rUrl.indexOf('-') + 1, rUrl.indexOf('?'));
+      let rCategoryId = rUrl.substring(rUrl.indexOf('=') + 1);
+      let rProduct = {
+        "id": rId ? parseInt(rId) : 0,
+        "title": rTitle,
+        "url": rUrl,
+        "price": rPrice ? parseFloat(rPrice).toFixed(2) : null,
+        "promotion-price": rPromotionPrice ? parseFloat(rPromotionPrice).toFixed(2) : null,
+        "sale-percent": rSalePercent ? parseFloat(rSalePercent).toFixed(2) : null,
+        "is-sale": rSalePercent ? true : false,
+        "thumbnail": rThumbnail,
+        "category_id": rCategoryId ? parseInt(rCategoryId) : 0,
+      }
+      relatedProducts.push(rProduct);
+    });
+    
+    let product = {
+      "title": title,
+      "price": price,
+      "promotion-price": promotionPrice ? parseFloat(promotionPrice).toFixed(2) : null,
+      "sale-percent": salePercent ? parseFloat(salePercent).toFixed(2) : null,
+      "is-sale": salePercent ? true : false,
+      "remaining-amount": remainingAmount ? parseInt(remainingAmount) : 0,
+      "thumbnail": thumbnail,
+      "likes": likes ? parseInt(likes) : 0,
+      "descriptions" : descriptions,
+      "tags": tags,
+      "gallery-thumbnails": galleryThumbnails,
+      "related-products": relatedProducts
+    };
+    return product;
+  }, selector);
+};
+
+module.exports = router;
+
+
+
---
+++ controllers/list.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const router = express();
+
+const openBrowser = async () => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false,
+  });
+  const page = await browser.newPage();
+  return {
+    browser,
+    page,
+  };
+};
+
+const closeBrowser = async ({ page, browser }) => {
+  await page.close();
+  await browser.close();
+};
+
+router.get('/products', async (req, res) => {
+  const { browser, page } = await openBrowser();
+
+  const products = await getProducts(page);
+
+  await closeBrowser({ page, browser });
+
+  if (products && products.length) {
+    return res.status(200).json(products);
+  }
+  else
+    return res.status(200).json({ message: "Nothing to crawl" });
+});
+
+const getProducts = async (page) => {
+  const url = 'http://www.bloodybunnystore.com/en';
+  const selector = '.item';
+  await page.goto(url);
+  await page.waitForSelector(selector);
+  return await page.evaluate(selector => {
+    let products = [];
+    let items = document.querySelectorAll(selector);
+    items.forEach((item) => {
+      let title = item.querySelector('h3')?.innerText.trim();
+      let url = item.querySelector('a')?.getAttribute('href').trim();
+      let price = item.querySelector('.list-price span')?.innerText.replace('฿', '').trim();
+      let promotionPrice = item.querySelector('.list-price del')?.innerText.replace('฿', '').trim();
+      let thumbnail = item.querySelector('.img-wrapper img')?.getAttribute('src').trim();
+      let salePercent = item.querySelector('.img-wrapper .label-sale')?.innerText.replace('-', '').replace('%','').trim();
+      let id = url.substring(url.indexOf('-') + 1, url.indexOf('?'));
+      let categoryId = url.substring(url.indexOf('=') + 1);
+      let product = {
+        "id": id ? parseInt(id) : 0,
+        "title": title,
+        "url": url,
+        "price": price ? parseFloat(price).toFixed(2) : null,
+        "promotion-price": promotionPrice ? parseFloat(promotionPrice).toFixed(2) : null,
+        "sale-percent": salePercent ? parseFloat(salePercent).toFixed(2) : null,
+        "is-sale": salePercent ? true : false,
+        "thumbnail": thumbnail,
+        "category_id": categoryId ? parseInt(categoryId) : 0,
+      }
+      products.push(product)
+    });
+    return products;
+  }, selector);
+};
+
+module.exports = router;
+
+
+
